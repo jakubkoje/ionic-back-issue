67b527038ad4c89db41128ad133fe7526bcaf229
Jakub Jelínek <jakubkoje@gmail.com>
2021-02-08 21:15:11 +0100

    init

---
+++ src/views/Page.vue
@@ -0,0 +1,44 @@
+<template>
+	<ion-page>
+		<ion-header>
+			<ion-toolbar>
+				<ion-buttons>
+					<ion-back-button></ion-back-button>
+				</ion-buttons>
+				<ion-title>Test</ion-title>
+			</ion-toolbar>
+		</ion-header>
+		<ion-content>
+			<ion-text>
+				<h1>Test</h1>
+			</ion-text>
+		</ion-content>
+	</ion-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import {
+	IonPage,
+	IonContent,
+	IonHeader,
+	IonTitle,
+	IonText,
+	IonToolbar,
+	IonButtons,
+	IonBackButton,
+} from '@ionic/vue';
+export default defineComponent({
+	name: 'Page',
+	components: {
+		IonPage,
+		IonContent,
+		IonHeader,
+		IonTitle,
+		IonText,
+		IonToolbar,
+		IonButtons,
+		IonBackButton,
+	},
+});
+</script>
